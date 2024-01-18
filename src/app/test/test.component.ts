3902f3d7bdd4ef7dbb68c75db15266c34604cab8
MahabirGupta <mahabirg81@gmail.com>
2024-01-18 10:05:03 +0800

    using class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,27 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { style } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `<h2>
             Welcome {{name}}
             </h2>
             
+            <h2 class="text-success">Codevolution</h2>
+            // Using class binding
+            <h2 [class]="successClass">Codevolution</h2>
+
+            <h2 class="text-special" [class]="successClass">Codevolution</h2>
+
+            <h2 [class.text-danger]="hasError">Codevolution</h2>
+            // using ngClass Directive
+            <h2 [ngClass]="messageClasses">Codevolution</h2>
+
             <h2>{{2+2}}</h2>
             <h2>{{"Welcome" + name}}</h2>
             <h2>{{name.length}}</h2>
@@ -29,13 +41,35 @@ import { DOCUMENT } from '@angular/common';
 
 
   `,
-  styles: []
+  styles: [`
+    .text-success{
+      color:green;
+    }
+    .text-danger{
+      color:red;
+    }
+    .text-special{
+      font-style:italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
   // declare the variable name
-  public name = "Tiger"
+  public name = "Tiger";
 
+  public successClass="text-success";
+  public hasError = true;
+
+  public isSpecial = true;
+
+  // Create an object
+  public messageClasses = {
+    "text-success":!this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.isSpecial
+
+  }
   // assigning global variable
   public siteUrl: string;
 
